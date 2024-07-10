a1e9a5760c1d1d71b475b4dd87a1577513bd11a2
Dominique Clark <melaydominique@gmail.com>
2024-07-10 13:22:51 -0400

    trouble rendering images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DigitalsMobile from './components/mobile/digitals';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world how are you?
-      </h1>
+      <h1>Hello world</h1>
+      <DigitalsMobile></DigitalsMobile>
     </div>
   );
 }
--- frontend/src/components/mobile/digitals.tsx
+++ frontend/src/components/mobile/digitals.tsx
@@ -1,2 +1,16 @@
 import React from 'react'
 
+const DigitalsMobile: React.FC = () => {
+
+
+
+    return (
+        <>
+        <div>
+            <img src={process.env.PUBLIC_URLsrc + '/images/Digital1.jpeg'} alt= 'Digital One'/>
+        </div>
+        </>
+    )
+}
+
+export default DigitalsMobile;
