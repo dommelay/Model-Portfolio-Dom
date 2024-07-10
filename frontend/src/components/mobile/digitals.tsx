cf9ae22d4d7b4858838097069c3516738ae0949b
Dominique Clark <melaydominique@gmail.com>
2024-07-10 13:54:45 -0400

    images rendering, digitals formatted for mobile view

--- frontend/src/components/mobile/digitals.tsx
+++ frontend/src/components/mobile/digitals.tsx
@@ -6,8 +6,11 @@ const DigitalsMobile: React.FC = () => {
 
     return (
         <>
-        <div>
-            <img src={process.env.PUBLIC_URLsrc + '/images/Digital1.jpeg'} alt= 'Digital One'/>
+        <div className='flex flex-col items-center justify-center'>
+            <img src='/images/Digital1.jpeg' alt= 'Digital Side' className='w-mobile mt-5'/>
+            <img src='/images/Digital2.jpeg'  alt= 'Digital Full' className='w-mobile mt-5'/>
+            <img src='/images/Digital3.jpeg'  alt= 'Digital Full' className='w-mobile mt-5'/>
+            <img src='/images/Digital4.jpeg'  alt= 'Digital Full' className='w-mobile mt-5'/>
         </div>
         </>
     )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    // margin: {
+
+    // },
     width: {
-      
+      'mobile': '325px'
     },
     extend: {},
   },
